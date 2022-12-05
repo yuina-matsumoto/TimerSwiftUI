c48b730aead1e0235041d49b337b0215f659e756
yuina <kaisyasenyou.m1@gmail.com>
2022-12-05 17:06:35 +0900

    初プッシュ

---
+++ TimerSwiftUI/TimerSwiftUI/TimerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerSwiftUIApp.swift
+//  TimerSwiftUI
+//
+//  Created by matsumotoyuina on 2022/12/05.
+//
+
+import SwiftUI
+
+@main
+struct TimerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
